50f67e00f1c111a1e8d167daef2a567b587597da
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-09-04 06:18:18 -0400

    Initial Commit

---
+++ Caster/CasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasterApp.swift
+//  Caster
+//
+//  Created by Andy Stewart on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caster
+//
+//  Created by Andy Stewart on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
